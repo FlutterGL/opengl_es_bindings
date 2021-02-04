c429ec4f118242ac6b42a38f26b9264e8e34a9b0
escamoteur <github@burkharts.net>
2021-02-04 11:39:06 +0100

    updated exports

--- lib/opengl_es_bindings.dart
+++ lib/opengl_es_bindings.dart
@@ -1,3 +1,4 @@
 library opengl_es_bindings;
 
-export 'package:opengl_es_bindings/src/bindings.dart';
+export 'package:opengl_es_bindings/src/egl_bindings.dart';
+export 'package:opengl_es_bindings/src/gles_bindings.dart';
